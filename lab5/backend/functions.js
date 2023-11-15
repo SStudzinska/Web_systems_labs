54c15527487d67eb408a03e47c06f97d7b3695e3
Rekkin0 <korzenmichal5@gmail.com>
2023-11-15 18:11:12 +0100

    quiz functions, styling functions + moved image changing by arrow keys to main.html

--- lab5/backend/functions.js
+++ lab5/backend/functions.js
@@ -1,32 +1,82 @@
+const images = ['images/athens1.png', 'images/athens2.png', 'images/athens3.png', 'images/theatre.jpg'];
 
-const images = ['images/athens1.png', 'images/athens2.png', 'images/athens3.png', 'images/aztec.jpg', 'images/colosseum.jpg', 'images/theatre.jpg'];
-let currentImageIndex = 0;
-
-function showImage(index) {
+function showImage(currentImageIndex) {
     const imageElement = document.getElementById('image');
-    imageElement.src = images[index];
+    imageElement.src = images[currentImageIndex];
 }
 
-
-window.addEventListener('keydown', (event) => {
-    if (event.defaultPrevented) {
-      return;
+function getAnswer(question) {
+    let questionElement = document.getElementById(question);
+    if (!questionElement) {
+        questionElement = document.querySelector(`input[name="${question}"]`);
     }
 
-    switch (event.key) {
-        case 'ArrowLeft':
-            currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-            showImage(currentImageIndex);
-            break;
-        case 'ArrowRight':
-            currentImageIndex = (currentImageIndex + 1) % images.length;
-            showImage(currentImageIndex);
-            break;
-        default:
-            break;
+    if (questionElement.type === 'radio') {
+        const checkedAnswer = document.querySelector(`input[name="${questionElement.name}"]:checked`);
+        return checkedAnswer ? checkedAnswer.value : null;
     }
-    event.preventDefault();
-},
-true);
+    if (questionElement.type === 'checkbox') {
+        return Array.from(document.querySelectorAll(`input[name="${question}"]:checked`))
+            .map(checkbox => checkbox.value);
+    } else {
+        return questionElement.value;
+    }
+}
 
-showImage(currentImageIndex);
+function arrayAnswerCorrect(a, b) {
+    return JSON.stringify(a.sort()) === JSON.stringify(b.sort());
+}
+
+function calculateScore(correctAnswers) {
+    const questions = Object.keys(correctAnswers);
+    let score = 0;
+
+    let i = 0;
+    do {
+        const question = questions[i];
+        const answer = getAnswer(question);
+        if (!answer) {
+            i++;
+            continue;
+        }
+        if (Array.isArray(correctAnswers[question])) {
+            const arrayAnswer = Array.isArray(answer) ? answer
+                .map(str => str.toLowerCase()) : [answer.toLowerCase()];
+            if (arrayAnswerCorrect(arrayAnswer, correctAnswers[question])) {
+                score++;
+            }
+        } else if (answer.toLowerCase() === correctAnswers[question]) {
+            score++;
+        }
+        i++;
+    } while (i < questions.length);
+    window.alert(`Your score is: ${score} out of ${questions.length}`);
+}
+
+function openFavoriteWebsite() {
+    const favoriteWebsite = document.getElementById('favorite-website').value;
+    if (favoriteWebsite)
+        window.open(favoriteWebsite);
+}
+
+const backgroundColorChanger = document.getElementById('background-color-changer');
+const textColorChanger = document.getElementById('text-color-changer');
+const fontFamilyChanger = document.getElementById('font-family-changer');
+
+function changeBackgroundColor() {
+    document.body.style.setProperty('background-color', backgroundColorChanger.value, 'important')
+}
+
+function changeTextColor() {
+    document.body.style.setProperty('color', textColorChanger.value, 'important')
+}
+
+function changeFontFamily() {
+    const elements = document.querySelectorAll('*');
+
+    let i = 0;
+    while (i < elements.length) {
+        elements[i].style.setProperty('font-family', fontFamilyChanger.value, 'important');
+        i++;
+    }
+}
