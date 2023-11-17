2d2c49d2e449d3e2b2b2c7366e96b8dcdaf0e775
SStudzinska <sstudzinska01@gmail.com>
2023-11-17 19:35:06 +0100

    item + namedItem properties

--- lab6/backend/collectionevents.js
+++ lab6/backend/collectionevents.js
@@ -6,8 +6,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const formsCollection = document.forms;
     const anchorsCollection = document.anchors;
 
+    const first_link = linksCollection.item(0).href;
+    const namedImage = document.images.namedItem("Parthenon");
+
     let dataButton = document.getElementById('dataButton');
     dataButton.addEventListener('click', function(){
-        document.getElementById('statisticsInfo').textContent = `In this website there are ${imagesCollection.length} images, ${linksCollection.length} links, ${formsCollection.length} forms and ${anchorsCollection.length} anchors.`;
+        const statisticsInfo = document.getElementById('statisticsInfo');
+        statisticsInfo.innerHTML = 
+        `In this website there are ${imagesCollection.length} images, ${linksCollection.length} links, ${formsCollection.length} forms, and ${anchorsCollection.length} anchors.<br>
+        First link: ${first_link}<br>
+        Parthenon image found: ${namedImage ? "Found" : "Not found"}`;
     });
 });
